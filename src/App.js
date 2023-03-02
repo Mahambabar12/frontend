94edeb38a93c427ced28c5194ccf49e24cc69816
Maham Babar <mahambabar11@gmail.com>
2023-03-02 05:56:34 -0800

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <a href="/">Amazona</a>
+      </header>
+      <main>
+      list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
